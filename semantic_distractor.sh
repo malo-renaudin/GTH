4aae0a000454b2e43a1c47cb5922a7d17aabd43f
malo-renaudin <malo.renaudn@hec.edu>
2026-05-12 18:29:07 +0200

    updated

--- semantic_distractor.sh
+++ semantic_distractor.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --account=ywa@h100
 #SBATCH --partition=gpu_p6
-#SBATCH --qos=qos_gpu_h100-dev
+#SBATCH --qos=qos_gpu_h100-t3
 #SBATCH --constraint=h100
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=24
 #SBATCH --hint=nomultithread
-#SBATCH --time=02:00:00
-#SBATCH --error=error_train_%a.log
-#SBATCH --output=output_train_%a.log
+#SBATCH --time=10:00:00
+#SBATCH --error=error_semantic_%a.log
+#SBATCH --output=output_semantic_%a.log
 
 source $WORK/miniconda3/etc/profile.d/conda.sh
 conda activate litgpt_jz
