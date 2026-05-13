ab25e31d545da2941e5ccabba9c60063f51f843f
malo-renaudin <malo.renaudn@hec.edu>
2026-05-13 12:15:11 +0200

    train

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=ywa@h100
 #SBATCH --partition=gpu_p6
-#SBATCH --qos=qos_gpu_h100-dev
+#SBATCH --qos=qos_gpu_h100-t3
 #SBATCH --constraint=h100
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=24
 #SBATCH --hint=nomultithread
-#SBATCH --time=02:00:00
+#SBATCH --time=10:00:00
 #SBATCH --error=error_train_%a.log
 #SBATCH --output=output_train_%a.log
 
