090901f4da226d13e171deace5534fe06e6ee3f3
malo-renaudin <malo.renaudn@hec.edu>
2026-07-08 08:10:46 +0200

    both augmentation

--- scripts/bash/train_streaming_dataset.sh
+++ scripts/bash/train_streaming_dataset.sh
@@ -9,14 +9,14 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --hint=nomultithread
 #SBATCH --time=20:00:00
-#SBATCH --error=results/baseline_wh_augmented/error_train_streaming%a.log
-#SBATCH --output=results/baseline_wh_augmented/output_train_streaming_%a.log
+#SBATCH --error=results/baseline_both_augmented/error_train_streaming%a.log
+#SBATCH --output=results/baseline_both_augmented/output_train_streaming_%a.log
 
 source $WORK/miniconda3/etc/profile.d/conda.sh
 conda activate litgpt_jz
 
 python scripts/train/streaming_dataset.py \
-   --output-dir results/baseline_wh_augmented\
+   --output-dir results/baseline_both_augmented\
    --probability-masses-orc eval_data/orc_test.txt \
    --probability-masses-wh  eval_data/wh_test.txt \
    --blimp-dir eval_data/blimp_data \
@@ -24,8 +24,8 @@ python scripts/train/streaming_dataset.py \
    --nested-outer eval_data/short_nested_outer_english.json \
    --filler-gap-orc eval_data/filler_gap_orc.csv \
    --filler-gap-wh eval_data/filler_gap_wh.csv \
-   --c4 0.95 \
-   --orc 0 \
+   --c4 0.9 \
+   --orc 0.05 \
    --wh 0.05 \
    --svo_wh 0 \
    --svo_orc 0 \
