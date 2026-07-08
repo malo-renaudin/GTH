1e2778eb442dd96fe1b39f6a0b7864c2082f05af
malo-renaudin <malo.renaudn@hec.edu>
2026-07-08 08:01:25 +0200

    denser ckpting + no augmentation at all

--- scripts/bash/train_streaming_dataset.sh
+++ scripts/bash/train_streaming_dataset.sh
@@ -9,10 +9,23 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --hint=nomultithread
 #SBATCH --time=20:00:00
-#SBATCH --error=results/final_training_test_hopefully_3/error_train_streaming%a.log
-#SBATCH --output=results/final_training_test_hopefully_3/output_train_streaming_%a.log
+#SBATCH --error=results/baseline_no_augmentation/error_train_streaming%a.log
+#SBATCH --output=results/baseline_no_augmentation/output_train_streaming_%a.log
 
 source $WORK/miniconda3/etc/profile.d/conda.sh
 conda activate litgpt_jz
 
-python scripts/train/streaming_dataset.py   --output-dir results/baseline_with_dense_window   --probability-masses-orc eval_data/orc_test.txt   --probability-masses-wh  eval_data/wh_test.txt   --blimp-dir eval_data/blimp_data   --nested-inner eval_data/short_nested_inner_english.json   --nested-outer eval_data/short_nested_outer_english.json   --filler-gap-orc eval_data/filler_gap_orc.csv   --filler-gap-wh eval_data/filler_gap_wh.csv
+python scripts/train/streaming_dataset.py \
+   --output-dir results/baseline_no_augmentation\
+   --probability-masses-orc eval_data/orc_test.txt \
+   --probability-masses-wh  eval_data/wh_test.txt \
+   --blimp-dir eval_data/blimp_data \
+   --nested-inner eval_data/short_nested_inner_english.json \
+   --nested-outer eval_data/short_nested_outer_english.json \
+   --filler-gap-orc eval_data/filler_gap_orc.csv \
+   --filler-gap-wh eval_data/filler_gap_wh.csv \
+   --c4 1 \
+   --orc 0 \
+   --wh 0 \
+   --svo_wh 0 \
+   --svo_orc 0 \
