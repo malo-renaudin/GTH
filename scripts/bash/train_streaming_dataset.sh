d1695325e31fc5ce2572447bf9567e2b37486b3b
malo-renaudin <malo.renaudn@hec.edu>
2026-07-09 15:03:00 +0200

    orc

--- scripts/bash/train_streaming_dataset.sh
+++ scripts/bash/train_streaming_dataset.sh
@@ -9,19 +9,19 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --hint=nomultithread
 #SBATCH --time=20:00:00
-#SBATCH --error=results/wh_augmented_full_datasets/error_train_streaming%a.log
-#SBATCH --output=results/wh_augmented_full_datasets/output_train_streaming_%a.log
+#SBATCH --error=results/orc_augmented_full_datasets/error_train_streaming%a.log
+#SBATCH --output=results/orc_augmented_full_datasets/output_train_streaming_%a.log
 
 source $WORK/miniconda3/etc/profile.d/conda.sh
 conda activate litgpt_jz
 
 python scripts/train/streaming_dataset.py \
-   --output-dir results/wh_augmented_full_datasets\
+   --output-dir results/orc_augmented_full_datasets\
    --blimp-dir eval_data/blimp_data \
    --nested-inner eval_data/short_nested_inner_english.json \
    --nested-outer eval_data/short_nested_outer_english.json \
    --filler-gap-orc eval_data/filler_gap_orc.csv \
    --filler-gap-wh eval_data/filler_gap_wh.csv \
    --c4 0.9 \
-   --orc 0 \
-   --wh 0.1 \
+   --orc 0.1 \
+   --wh 0 \
